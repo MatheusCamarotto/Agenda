d535e122f5733ed6af775c6e729731d56b7a4c76
camarottoMD <matheus.domingues2@estudante.ifms.edu.br>
2026-02-09 18:01:11 -0300

    feat: adicionando funcionalidade de favoritar

--- functions.py
+++ functions.py
@@ -4,12 +4,20 @@ def salvar_contato():
     nome = input("Insira o nome do contato: ").lower().strip()
     telefone = input("Insira o telefone do contato: ")
     email = input("Insira o email do contato: ")
-
+    favorito = input("Deseja favoritar o contato?(s = sim, n = não) ").lower().strip()
     contatos[nome] = {'telefone': telefone, 'email': email}
+    if favorito == 's':
+        contatos[nome].update({'favorito': True})
+    elif favorito == 'n':
+        contatos[nome].update({'favorito': False})
+    else:
+        print("Opção invalida!")
+
 
 def visualizar_contatos():
     for contato, infos in contatos.items():
         telefone = infos.get('telefone')
         email = infos.get('email')
-        print(f'{contato} \nTelefone: {telefone} \nEmail: {email}')
+        favorito = 'Favoritado' if infos['favorito'] == True else ''
+        print(f'{contato} \nTelefone: {telefone} \nEmail: {email} \n{favorito}')
 
