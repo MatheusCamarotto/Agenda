647abfc425bf65538a756e4a79de7be2407b2e15
CmD_! <matheus.domingues2@estudante.ifms.edu.br>
2026-02-17 16:50:11 -0300

    feat: projeto finalizado como V1

--- functions.py
+++ functions.py
@@ -2,16 +2,19 @@ contatos = {}
 
 def salvar_contato():
     nome = input("Insira o nome do contato: ").lower().strip()
-    telefone = input("Insira o telefone do contato: ")
-    email = input("Insira o email do contato: ")
-    favorito = input("Deseja favoritar o contato?(s = sim, n = não) ").lower().strip()
+    telefone = input("Insira o telefone do contato: ").strip()
+    email = input("Insira o email do contato: ").strip()
     contatos[nome] = {'telefone': telefone, 'email': email}
-    if favorito == 's':
-        contatos[nome].update({'favorito': True})
-    elif favorito == 'n':
-        contatos[nome].update({'favorito': False})
-    else:
-        print("Opção invalida!")
+    while True:
+        favorito = input("Deseja favoritar o contato?(s = sim, n = não) ").lower().strip()
+        if favorito == 's':
+            contatos[nome].update({'favorito': True})
+            break
+        elif favorito == 'n':
+            contatos[nome].update({'favorito': False})
+            break
+        else:
+            print("Opção invalida!")
 
 
 def visualizar_contatos():
@@ -19,5 +22,49 @@ def visualizar_contatos():
         telefone = infos.get('telefone')
         email = infos.get('email')
         favorito = 'Favoritado' if infos['favorito'] == True else ''
+        print("-------------------------------")
         print(f'{contato} \nTelefone: {telefone} \nEmail: {email} \n{favorito}')
+
+def editar_contatos():
+    while True:
+        visualizar_contatos()
+        op = input("Qual contato deseja editar? ")
+        if op in contatos:
+            op_edicao = input(f"1 - Alterar nome \n2 - Alterar número \n3 - Alterar Email \n4 - (des)Favoritar\n5 - Apagar Contato\n")
+            match op_edicao:
+                case '1':
+                    new_name = input("Nome: ")
+                    contatos[new_name] = contatos.pop(op)
+                    print("Nome atualizado com sucesso!")
+                    break
+                case '2':
+                    new_number = input("N° de Telefone: ")
+                    contatos[op]['telefone'] = new_number
+                    print("Numero atualizado com sucesso!")
+                    break
+                case '3':
+                    new_email = input("Endereço de Email: ")
+                    contatos[op]['email'] = new_email
+                    print("Email atualizado com sucesso!")
+                    break
+                case '4':
+                    contatos[op]['favorito'] = not contatos[op].get('favorito')
+                    print("Atualizado com sucesso!")
+                    break
+                case '5':
+                    contatos.pop(op)
+                    break
+                case _:
+                    print("Opção invalida!")
+        else:
+            print("Esse contato não existe!")
+                 
+def visualizar_favoritos():
+    for contato, infos in contatos.items():
+        telefone = infos.get('telefone')
+        email = infos.get('email')
+        favorito = 'Favoritado' if infos['favorito'] == True else ''
+        if infos['favorito'] == True:
+            print("-------------------------------")
+            print(f'{contato} \nTelefone: {telefone} \nEmail: {email} \n{favorito}')
 
--- main.py
+++ main.py
@@ -6,7 +6,8 @@ def main():
         print("1 - Salvar Novo Contato")
         print("2 - Visualizar Contatos")
         print("3 - Visualizar Favoritos")
-        print("4 - Sair")
+        print("4 - Editar Contatos")
+        print("5 - Sair")
         print("-------------------------------")
         op = input("Selecione a opção desejada: ")
         
@@ -18,8 +19,11 @@ def main():
             case '3':
                 visualizar_favoritos()
             case '4':
+                editar_contatos()
+            case '5':
                 exit()
-
+            case _:
+                print("Opção invalida!")
 
 if __name__ == "__main__":
     main()
